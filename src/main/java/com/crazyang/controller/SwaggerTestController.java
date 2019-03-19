2a596363e563644c3a4120f2512e0ff89b98735a
zhouyangok <zhouyangx99@163.com>
2019-03-19 17:22:16 +0800

    阿里云服务器部署

--- src/main/java/com/crazyang/controller/SwaggerTestController.java
+++ src/main/java/com/crazyang/controller/SwaggerTestController.java
@@ -2,11 +2,9 @@ package com.crazyang.controller;
 
 import com.crazyang.core.util.AjaxObject;
 import com.crazyang.entity.SwaggerTest;
-import com.crazyang.service.LearnService;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
